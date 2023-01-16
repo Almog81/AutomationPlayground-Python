f7fd177443c1beba1ca048864c2dff278c6d88b8
Almog81 <almog81@gmail.com>
2023-01-16 06:17:49 +0200

    Add a Base File

--- all_the_test.py
+++ all_the_test.py
@@ -1,26 +1,6 @@
-import time
-
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common import alert
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 15)
-action = ActionChains(driver)
-
-
-@pytest.fixture(autouse=True, scope='session')
-def init_test():
-    driver.maximize_window()
-    driver.implicitly_wait(20)
-    yield
-    # time.sleep(3)
-    driver.quit()
+from base import *
 
 
 def test01_dynamic_id():
---
+++ base.py
@@ -0,0 +1,19 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 15)
+action = ActionChains(driver)
+
+
+@pytest.fixture(autouse=True, scope='session')
+def init_test():
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+    yield
+    # time.sleep(3)
+    driver.quit()
